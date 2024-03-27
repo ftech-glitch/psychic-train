99a373ad0b72abc30c388b2668b84ab18cc1ae9d
andreteo <andreteosy@gmail.com>
2024-03-27 12:58:58 +0800

    Import application from Hopspot https://github.com/ftech-glitch/the-hopspot/tree/main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Search from "./components/Search";
+import Add from "./components/Add";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h2>GA SEI</h2>
-    </div>
+    <>
+      <NavBar></NavBar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="add" element={<Add />} />
+        <Route path="search" element={<Search />} />
+      </Routes>
+    </>
   );
 }
 
